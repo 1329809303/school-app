be1a2f30cab7f37829b510fa8eb86a20f3c6f10f
1329809303 <49605663+1329809303@users.noreply.github.com>
2020-05-28 12:25:03 +0800

    Update LoginActivity.java
    与后端数据库相连

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,8 +2,10 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,6 +16,15 @@ import com.example.myapplication.Dao.article;
 import java.util.ArrayList;
 import java.util.List;
 import com.example.myapplication.Util.articleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class LoginActivity extends AppCompatActivity {
     private List<article> articleList=new ArrayList<>();
     ImageButton to_publish;
@@ -23,20 +34,53 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
         init();
-        initArticle(articleList);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    FormBody.Builder params=new FormBody.Builder();
+                    params.add("username","xhc");
+                    OkHttpClient client =new OkHttpClient();
+                    Request request=new Request.Builder()
+                            .url("http://192.168.0.111:8086/rest/GetArticle")
+                            .post(params.build())
+                            .build();
+                    Response response =client.newCall(request).execute();
+                    String responseData=response.body().string();
+                    JSONArray jsonArray=new JSONArray(responseData);
+                    for(int i=0;i<jsonArray.length();i++){
+                        JSONObject jsonObject=jsonArray.getJSONObject(i);
+                        article article=new article(jsonObject.getString("username"),jsonObject.getString("title"),jsonObject.getString("time"),jsonObject.getString("content"),R.drawable.image,R.drawable.question,R.drawable.download);
+                        articleList.add(article);
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            recyclerView=(RecyclerView)findViewById(R.id.recy_list);
+                            LinearLayoutManager layoutManager=new LinearLayoutManager(LoginActivity.this);
+                            recyclerView.setLayoutManager(layoutManager);
+                            articleAdapter adapter=new articleAdapter(articleList,LoginActivity.this);
+                            recyclerView.setAdapter(adapter);
+                            Toast.makeText(LoginActivity.this,"发布成功！",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }catch(Exception e){
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(LoginActivity.this,"发布失败！",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+            }
+        }).start();
         recyclerView=(RecyclerView)findViewById(R.id.recy_list);
-        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(LoginActivity.this);
         recyclerView.setLayoutManager(layoutManager);
-        articleAdapter adapter=new articleAdapter(articleList,this);
+        articleAdapter adapter=new articleAdapter(articleList,LoginActivity.this);
         recyclerView.setAdapter(adapter);
-    }
-    private void initArticle(List<article> articles){
-        article a1 = new article("发帖人:李华", "求解一道数分题", "05-20", "求求各位大佬帮忙解决一下问题", R.drawable.image, R.drawable.question,R.drawable.download);
-        article a2 = new article("发帖人:李华", "求解一道数分题", "05-20", "求求各位大佬帮忙解决一下问题", R.drawable.image, R.drawable.question,R.drawable.download);
-        article a3 = new article("发帖人:李华", "求解一道数分题", "05-20", "求求各位大佬帮忙解决一下问题", R.drawable.image, R.drawable.question,R.drawable.download);
-        articles.add(a1);
-        articles.add(a2);
-        articles.add(a3);
     }
     private void init() {
        to_publish=findViewById(R.id.to_publish);
